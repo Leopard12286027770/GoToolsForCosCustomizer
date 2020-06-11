43d110998b2a5ca295747840eee5829d7a981da1
Leopard12286027770 <gaohe124@126.com>
2020-06-10 23:11:01 -0400

    changed read_disk_stat to read_disk_size

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
-import (
-	"GoToolsForCosCustomizer/tools"
-	"fmt"
-)
+import "GoToolsForCosCustomizer/tools"
 
 func main() {
 	// tools.Read_whole_disk()
-	// tools.Read_disk_stat()
-	fmt.Println("sdb1::")
 	tools.Read_disk_size("/dev/sdb1")
-	fmt.Println("sdb::")
 	tools.Read_disk_size("/dev/sdb")
-
 }
--- tools/read_disk_size.go
+++ tools/read_disk_size.go
@@ -2,18 +2,26 @@ package tools
 
 import (
 	"fmt"
-	"os"
+	"syscall"
 )
 
-//Read_disk_size read a disk size
+//Read_disk_size read disk size
 func Read_disk_size(disk string) {
-	file, err := os.Open(disk)
-	if Check(err) {
+	var stat syscall.Statfs_t
+	err := syscall.Statfs(disk, &stat)
+	if err != nil {
+		fmt.Println(err.Error())
 		return
 	}
-	stat, err := file.Stat()
-	if Check(err) {
-		return
-	}
-	fmt.Println(stat.Size())
+
+	// Available blocks * size per block = available space in bytes
+	size := stat.Blocks * uint64(stat.Bsize)
+	ava := stat.Bavail * uint64(stat.Bsize)
+	fmt.Println("Disk: ", disk)
+
+	fmt.Println("Disk size: ", size)
+	fmt.Println("Disk available: ", ava)
+	fmt.Println("Disk used: ", size-ava)
+	fmt.Println()
+
 }
