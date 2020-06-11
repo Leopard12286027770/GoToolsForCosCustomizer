70cce7201715583e383c921817ecce7050bf2764
Leopard12286027770 <gaohe124@126.com>
2020-06-11 11:10:17 -0400

    correct function name Cap

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
+import "GoToolsForCosCustomizer/tools"
+
 func main() {
-	// tools.Read_whole_disk("/dev/sdb1")
+	tools.Read_whole_disk("/dev/sdb1")
 	// tools.Read_disk_size("/dev/sdb1")
 	// tools.Read_disk_size("/dev/sdb")
 	// tools.Read_disk_size("/dev/sda1")
 	// tools.Read_disk_size("/dev/sda15")
 	// tools.Read_disk_size("/dev/sda")
-	tools.seek_disk_end("/dev/sdb1")
-	tools.seek_disk_end("/dev/sdb")
+	tools.Seek_disk_end("/dev/sdb1")
+	tools.Seek_disk_end("/dev/sdb")
 }
