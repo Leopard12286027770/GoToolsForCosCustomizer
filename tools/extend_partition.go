eb200dabcbcc2d9bcc395e52431c97b09e06f884
Leopard12286027770 <gaohe124@126.com>
2020-06-12 18:20:37 -0400

    asd

--- tools/extend_partition.go
+++ tools/extend_partition.go
@@ -8,7 +8,7 @@ import (
 //ExtendPartition extends a partition to a specific end sector
 func ExtendPartition(disk, partNum string, end int) {
 	cmd := string("sfdisk --dump ") + disk + " > extend_partition_tmp"
-	exec.Command("/bin/bash", "-c", cmd)
+	exec.Command("/bin/bash", "-c", cmd).Run()
 	part := disk + partNum
 	fmt.Println(part)
 }
